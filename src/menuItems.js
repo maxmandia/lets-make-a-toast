04006cdf79e75c1201f013fb3b48bfbdf24b1e1b
Max Mandia <mmandia511@gmail.com>
2022-09-29 21:25:46 -0400

    saving work

--- src/menuItems.js
+++ src/menuItems.js
@@ -3,10 +3,12 @@ export const menuItems = [
     name: "The Ry(e)an",
     desc: "The best one idk just get this one trust me.",
     creator: "Ryan",
+    price: "$24.99",
   },
   {
     name: "Toastally",
     desc: "The best avocado (forsureee).",
     creator: "Ally",
+    price: "$19.99",
   },
 ];
--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -9,19 +9,39 @@ import {
 import React from "react";
 import theme from "../theme";
 import { menuItems } from "../menuItems";
-import ToastIMG from "../../assets/toast.jpg";
+import ToastIMG from "../../assets/toast.svg";
 const MenuScreen = () => {
   return (
     <>
       <View style={styles.container}>
         <View style={styles.header} />
-        <Image style={styles.img} source={ToastIMG} />
-        <FlatList
-          data={menuItems}
-          renderItem={({ item, index }) => {
-            return <View key={index}>{<Text>{item.name}</Text>}</View>;
-          }}
-        />
+        <View style={styles.restContainer}>
+          <ToastIMG width={110} height={110} />
+          <Text style={styles.restName}>Let's Make a Toast!</Text>
+          <Text style={styles.restDesc}>
+            Serving up all things toast since 1998. You’d toast to that, right?
+          </Text>
+        </View>
+        <View style={styles.flContainer}>
+          <FlatList
+            data={menuItems}
+            renderItem={({ item, index }) => {
+              return (
+                <View style={styles.itemContainer} key={index}>
+                  <Text style={styles.name}>{item.name}</Text>
+                  <View style={styles.itemDesc}>
+                    <Text style={styles.price}>{item.price}</Text>
+                    <View style={styles.dot} />
+                    <Text style={styles.creator}>
+                      created by {item.creator}
+                    </Text>
+                  </View>
+                  <Text style={styles.descText}>{item.desc}</Text>
+                </View>
+              );
+            }}
+          />
+        </View>
       </View>
     </>
   );
@@ -39,12 +59,64 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "15%",
   },
-  img: {
+  restContainer: {
     position: "absolute",
-    textAlign: "center",
-    top: "11%",
+    alignItems: "center",
+    top: "8%",
     alignSelf: "center",
-    height: 110,
-    width: 110,
+  },
+  restName: {
+    marginTop: "2%",
+    fontFamily: "SF-Bold",
+    color: "#5F4848",
+    fontSize: 26,
+  },
+  restDesc: {
+    marginTop: "1%",
+    fontFamily: "SF-Med",
+    color: "#5F4848",
+    fontSize: 18,
+    width: "60%",
+    textAlign: "center",
+  },
+
+  //flatlist
+  flContainer: {
+    marginTop: "45%",
+    marginLeft: "8%",
+  },
+  itemContainer: {
+    marginBottom: "10%",
+  },
+  name: {
+    color: "#5F4848",
+    fontFamily: "SF-Bold",
+    fontSize: 18,
+  },
+  itemDesc: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: "3%",
+    textAlign: "left",
+  },
+  price: {
+    color: "#5F4848",
+  },
+  dot: {
+    height: 5,
+    width: 5,
+    borderRadius: 5,
+    backgroundColor: "#5F4848",
+    marginLeft: "2%",
+  },
+  creator: {
+    color: "#5F4848",
+    textAlign: "left",
+    marginLeft: "2%",
+    fontStyle: "italic",
+  },
+  descText: {
+    color: "#5F4848",
+    marginTop: "3%",
   },
 });
